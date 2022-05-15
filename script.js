b8ba80eb06593e28da5976eba6ef8006f4c4acf6
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2022-05-15 17:51:41 +0400

    added welcome screen, modified typography sizings

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-import { slides } from './slides.js';
+import { slides } from "./slides.js";
 
-const slideContainer = document.querySelector('.slide-container');
-const slideNumber = document.querySelector('.slide-number').querySelector('h1');
-const slideName = document.querySelector('.slide-name').querySelector('h1');
-const pageItems = document.querySelectorAll('.page-item');
+const slideContainer = document.querySelector(".slide-container");
+const slideNumber = document.querySelector(".slide-number").querySelector("h1");
+const slideName = document.querySelector(".slide-name").querySelector("h1");
+const pageItems = document.querySelectorAll(".page-item");
+const welcome = document.querySelector(".welcome");
 let numPage = 1;
 
 const renderPage = () => {
@@ -11,46 +12,65 @@ const renderPage = () => {
     <div class="slide-content slide-${numPage}">
       <ul class="slide-rows">
       ${slides[numPage - 1].listItems
-        .map(li => {
+        .map((li) => {
           return `<li class="slide-row"><p>${li}</p></li>`;
         })
-        .join('')}
+        .join("")}
       </ul>
     </div>
   `;
 
   slideContainer.innerHTML = ``;
-  slideContainer.insertAdjacentHTML('afterbegin', html);
+  slideContainer.insertAdjacentHTML("afterbegin", html);
   slideNumber.textContent = numPage;
   slideName.textContent = slides[numPage - 1].name;
 
-  pageItems.forEach(pageItem => {
+  pageItems.forEach((pageItem) => {
     const id = pageItem.dataset.id;
     if (id <= numPage) {
-      pageItem.classList.add('seen');
+      pageItem.classList.add("seen");
     } else {
-      pageItem.classList.remove('seen');
+      pageItem.classList.remove("seen");
     }
   });
 };
 
-renderPage();
+const init = () => {
+  welcome.classList.remove("invisible");
+  welcome.classList.remove("hidden");
+  renderPage();
+};
 
-document.addEventListener('keydown', e => {
+init();
+
+const getStartedHandler = (e) => {
+  welcome.classList.add("invisible");
+  setTimeout(() => {
+    welcome.classList.add("hidden");
+  }, 1000);
+};
+document.addEventListener("click", getStartedHandler);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getStartedHandler(e);
+  }
+});
+
+document.addEventListener("keydown", (e) => {
   numPage;
-  if (e.key === 'ArrowRight' || e.key === 'ArrowDown') {
+  if (e.key === "ArrowRight" || e.key === "ArrowDown") {
     if (numPage === 6) return;
     numPage++;
   }
-  if (e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
+  if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
     if (numPage === 1) return;
     numPage--;
   }
   renderPage();
 });
 
-pageItems.forEach(item => {
-  item.addEventListener('click', () => {
+pageItems.forEach((item) => {
+  item.addEventListener("click", () => {
     numPage = item.dataset.id;
     renderPage();
   });
