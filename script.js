17b7ccce5d4791c4bff429554c1bbe92a6fc4d9a
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2022-05-15 14:42:26 +0400

    added slides' content

--- script.js
+++ script.js
@@ -1,65 +1,10 @@
+import { slides } from './slides.js';
+
 const slideContainer = document.querySelector('.slide-container');
 const slideNumber = document.querySelector('.slide-number').querySelector('h1');
 const slideName = document.querySelector('.slide-name').querySelector('h1');
 const pageItems = document.querySelectorAll('.page-item');
 let numPage = 1;
-
-const slides = [
-  {
-    name: 'Analysis',
-    listItems: [
-      'Product Owner 1',
-      'Project Manager 1',
-      'Business Analyst 1',
-      'CTO 1',
-    ],
-  },
-  {
-    name: 'Design',
-    listItems: [
-      'Product Owner 2',
-      'Project Manager 2',
-      'Business Analyst 2',
-      'CTO 2',
-    ],
-  },
-  {
-    name: 'Development',
-    listItems: [
-      'Product Owner 3',
-      'Project Manager 3',
-      'Business Analyst 3',
-      'CTO 3',
-    ],
-  },
-  {
-    name: 'Testing',
-    listItems: [
-      'Product Owner 4',
-      'Project Manager 4',
-      'Business Analyst 4',
-      'CTO 4',
-    ],
-  },
-  {
-    name: 'Deployment',
-    listItems: [
-      'Product Owner 5',
-      'Project Manager 5',
-      'Business Analyst 5',
-      'CTO 5',
-    ],
-  },
-  {
-    name: 'Maintenace',
-    listItems: [
-      'Product Owner 6',
-      'Project Manager 6',
-      'Business Analyst 6',
-      'CTO 6',
-    ],
-  },
-];
 
 const renderPage = () => {
   const html = `
