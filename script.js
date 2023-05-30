a61206da1191352bde50d84d33decfc47e510d16
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2023-05-30 11:26:43 +0400

    cleaning up and refactoring

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-import { slides } from './slides.js';
-
+import data from './slides.json' assert { type: 'json' };
+const { slides } = data;
 const slideContainer = document.querySelector('.slide-container');
 const slideNumber = document.querySelector('.slide-number').querySelector('h1');
 const slideName = document.querySelector('.slide-name').querySelector('h1');
 const pageItems = document.querySelectorAll('.page-item');
 const welcome = document.querySelector('.welcome');
-
 let numPage = 1;
 
 const renderPage = () => {
@@ -13,9 +12,7 @@ const renderPage = () => {
     <div class="slide-content slide-${numPage}">
       <ul class="slide-rows">
       ${slides[numPage - 1].listItems
-        .map(li => {
-          return `<li class="slide-row"><p>${li}</p></li>`;
-        })
+        .map(li => `<li class="slide-row"><p>${li}</p></li>`)
         .join('')}
       </ul>
     </div>
@@ -28,11 +25,8 @@ const renderPage = () => {
 
   pageItems.forEach(pageItem => {
     const id = pageItem.dataset.id;
-    if (id <= numPage) {
-      pageItem.classList.add('seen');
-    } else {
-      pageItem.classList.remove('seen');
-    }
+    if (id <= numPage) pageItem.classList.add('seen');
+    else pageItem.classList.remove('seen');
   });
 };
 
@@ -46,38 +40,26 @@ init();
 
 const getStartedHandler = e => {
   welcome.classList.add('invisible');
-  setTimeout(() => {
-    welcome.classList.add('hidden');
-  }, 1000);
+  setTimeout(() => welcome.classList.add('hidden'), 1e3);
 };
 document.addEventListener('click', getStartedHandler);
-document.addEventListener('keydown', e => {
-  if (e.key === 'Enter') {
-    getStartedHandler(e);
-  }
-});
+document.addEventListener(
+  'keydown',
+  e => e.key === 'Enter' && getStartedHandler(e)
+);
 
 document.addEventListener('keydown', e => {
-  // const navigationKeys = ["ArrowRight", "ArrowDown", "PageDown", "ArrowLeft", "ArrowUp", "PageUp", "Home", "End"];
-  // if (!navigationKeys.includes(e.key)) return;
-  const isNextKey =
-    e.key === 'ArrowRight' || e.key === 'ArrowDown' || e.key === 'PageDown';
-  const isPreviousKey =
-    e.key === 'ArrowLeft' || e.key === 'ArrowUp' || e.key === 'PageUp';
+  const isNextKey = ['ArrowRight', 'ArrowDown', 'PageDown'].includes(e.key);
+  const isPreviousKey = ['ArrowLeft', 'ArrowUp', 'PageUp'].includes(e.key);
   const isHomeKey = e.key === 'Home';
   const isEndKey = e.key === 'End';
-
-  if (isNextKey && numPage !== 6) {
-    numPage++;
-  } else if (isPreviousKey && numPage !== 1) {
-    numPage--;
-  } else if (isHomeKey && numPage !== 1) {
-    numPage = 1;
-  } else if (isEndKey && numPage !== 6) {
-    numPage = 6;
-  } else {
-    return;
-  }
+  const ACTIONS = {
+    [isNextKey && numPage !== 6]: () => numPage++,
+    [isPreviousKey && numPage !== 1]: () => numPage--,
+    [isHomeKey && numPage !== 1]: () => (numPage = 1),
+    [isEndKey && numPage !== 6]: () => (numPage = 6),
+  };
+  ACTIONS['true']?.();
   renderPage();
 });
 
@@ -99,7 +81,6 @@ const storyRight = document.querySelector('.story-right');
       numPage--;
       renderPage();
     } else return;
-    console.log('LEFT');
   });
 });
 
@@ -109,6 +90,5 @@ const storyRight = document.querySelector('.story-right');
       numPage++;
       renderPage();
     } else return;
-    console.log('RIGHT');
   });
 });
